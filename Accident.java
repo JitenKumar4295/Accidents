9cfef5b9bf77e9c95a0cdc3e550231f4e0548bfa
JitenKumar4295 <71107552+JitenKumar4295@users.noreply.github.com>
2026-04-25 08:18:43 +0530

    Refactor Accident class and fix constructor definition

--- Accident.java
+++ Accident.java
@@ -1,13 +1,13 @@
-class Accident{
-private no_of_injured = 0;
-public Accidents(int no_of_injured){
-this.no_of_injured = no_of_injured;
-}
-public String getStringAccident(){
-if(no_of_injured>=10)
-return "Disaster";
-else if(no_of_injured>3)
-return "Major Accident";
-else return "Fatal Accident";
-}
-}
+public class Accident{
+        private int no_of_injured = 0;
+        public Accident(int no_of_injured){
+            this.no_of_injured = no_of_injured;
+            }
+        public String getStringAccident(){
+            if(no_of_injured>=10)
+                return "Disaster";
+            else if(no_of_injured>3)
+                return "Major Accident";
+            else return "Fatal Accident";
+            }
+    }
